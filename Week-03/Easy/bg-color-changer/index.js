5aa3e2e36892754aca7d6307ff320dd0864eb005
Munadil16 <munadilxd@gmail.com>
2024-09-06 15:51:53 +0530

    feat: Add Week-03/Easy/bg-color-changer solution files

---
+++ Week-03/Easy/bg-color-changer/index.js
@@ -0,0 +1,8 @@
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const color = button.getAttribute("id");
+    document.body.style.backgroundColor = color;
+  });
+});
